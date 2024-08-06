fb4ccc23eb6f3f0f5e44d29243af89a5ee1d1b9c
Mariia Kalashnikova <mary03079@gmail.com>
2024-08-06 16:03:35 +0200

    Add axios

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './SignUp.css';
+import { todoAPI } from '../api/todoAPI';
 
 function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+
+    todoAPI.post('/users', {
+      user: {
+        email: email,
+        password: password,
+        name: 'John',
+        last_name: 'Doe'
+      }
+    });
+  };
+
   return (
     <div className="sign-up__page">
       <section className="sign-up">
         <h2 className="sign-up-page-name">
           Create account
         </h2>
-        <form className="sign-up-form">
+        <form className="sign-up-form" onSubmit={handleSubmit}>
           <div className="form-field">
             <label htmlFor="user-email" className="form-field-lable">Email</label>
-            <input type="email" id="user-email" className="form-input" required />
+            <input type="email" id="user-email" className="form-input" required onChange={(evt) => setEmail(evt.target.value)} />
           </div>
 
           <div className="form-field">
             <label htmlFor="user-password" className="form-field-lable">Password</label>
-            <input type="password" id="user-password" className="form-input" required />
+            <input type="password" id="user-password" className="form-input" required onChange={(evt) => setPassword(evt.target.value)} />
           </div>
 
           <button type="submit" className="form-btn">
