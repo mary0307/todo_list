7938d9fea379de7ba71c51f64f6b7c396e4840b7
Mariia Kalashnikova <mary03079@gmail.com>
2024-08-08 17:29:08 +0200

    frontend validation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import './App.css';
 import SignUp from './pages/SignUp';
+import './App.css'; // Keep it the last import for css ordering
 
 function App() {
   return (
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,23 +9,72 @@ function SignUp() {
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [nameError, setNameError] = useState('');
+  const [lastNameError, setLastNameError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [passwordConfirmationError, setPasswordConfirmationError] = useState('');
+
+
+  const [genericError, setGenericError] = useState('');
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    if (password !== confirmPassword ) {
-      alert('Passwords do not match');
+    setGenericError('');
+    setNameError('');
+    setLastNameError('');
+    setPasswordError('');
+    setEmailError('');
+    setPasswordConfirmationError('');
+
+    if (email === '') {
+      setEmailError('Enter your email');
+      setGenericError('Pleas fill in all field');
       return;
     }
 
-    todoAPI.post('/users', {
-      user: {
-        email: email,
-        password: password,
-        name: name,
-        last_name: lastName
-      }
-    });
+    if (password === '') {
+      setPasswordError('Enter your password');
+      setGenericError('Pleas fill in all field');
+      return;
+    }
+
+    if (name === '') {
+      setNameError('Enter your name');
+      setGenericError('Pleas fill in all field');
+      return;
+    }
+
+    if (lastName === '') {
+      setLastNameError('Enter your last name');
+      setGenericError('Pleas fill in all field');
+      return;
+    }
+
+    if (password !== confirmPassword ) {
+      setPasswordConfirmationError('Passwords do not match');
+      return;
+    }
+
+    if (!email.length || !password.length || !name.length || !lastName.length || !confirmPassword.length) {
+      setGenericError('Pleas fill in all field');
+      return;
+    }
+
+    todoAPI
+      .post('/users', {
+        user: {
+          email: email,
+          password: password,
+          name: name,
+          last_name: lastName
+        }
+      })
+      .catch((err) => {
+        console.error('Sign up request failed', err);
+        setGenericError('Unexpected error, please try again later');
+      });
   };
 
   return (
@@ -37,37 +86,101 @@ function SignUp() {
         <form className="sign-up-form" onSubmit={handleSubmit}>
           <div className="form-field">
             <label htmlFor="user-email" className="form-field-lable">Email</label>
-            <input type="email" id="user-email" className="form-input" required
-                   onChange={(evt) => setEmail(evt.target.value)} />
+            <input type="email" id="user-email"
+                   className={`form-input ${emailError ? `border-red-500` : ''}`}
+                   required
+                   onChange={(evt) => {
+                     setEmailError('');
+                     setEmail(evt.target.value);
+                   }} />
+            {emailError &&
+              <div className="text-red-500">
+                { emailError }
+              </div>
+            }
           </div>
 
           <div className="form-field">
             <label htmlFor="user-name" className="form-field-lable">Name</label>
-            <input type="text" id="user-name" className="form-input" required
-                   onChange={(evt) => setName(evt.target.value)} />
+            <input type="text" id="user-name"
+                   className={`form-input ${nameError ? `border-red-500` : ''}`}
+                   required
+                   onChange={(evt) => {
+                     setNameError('');
+                     setName(evt.target.value)
+                   }} />
+            {nameError &&
+              <div className="text-red-500">
+                { nameError }
+              </div>
+            }
           </div>
 
           <div className="form-field">
             <label htmlFor="user-last-name" className="form-field-lable">Surname</label>
-            <input type="text" id="user-last-name" className="form-input" required
-                   onChange={(evt) => setLastName(evt.target.value)} />
+            <input type="text" id="user-last-name"
+                   className={`form-input ${lastNameError ? `border-red-500` : ''}`}
+                   required
+                   onChange={(evt) => {
+                     setLastNameError('');
+                     setLastName(evt.target.value)
+                   }} />
+            {lastNameError &&
+              <div className="text-red-500">
+                { lastNameError }
+              </div>
+            }
           </div>
 
           <div className="form-field">
             <label htmlFor="user-password" className="form-field-lable">Password</label>
-            <input type="password" minLength="6" id="user-password" className="form-input" required
-                   onChange={(evt) => setPassword(evt.target.value)} />
+            <input type="password" minLength="6" id="user-password"
+                   className={`form-input ${passwordError ? `border-red-500` : ''}`}
+                   required
+                   onChange={(evt) => {
+                     setPasswordError('');
+                     setPassword(evt.target.value)
+                     if (evt.target.value === confirmPassword) {
+                       setPasswordConfirmationError('');
+                     }
+                   }} />
+            {passwordError &&
+              <div className="text-red-500">
+                { passwordError }
+              </div>
+            }
           </div>
 
           <div className="form-field">
             <label htmlFor="user-confirm-password" className="form-field-lable">Confirm password</label>
-            <input type="password" minLength="6" id="user-confirm-password" className="form-input" required
-                   onChange={(evt) => setConfirmPassword(evt.target.value)} />
+            <input
+              type="password"
+              minLength="6"
+              id="user-confirm-password"
+              className={`form-input ${passwordConfirmationError ? `border-red-500` : ''}`}
+              required
+              onChange={(evt) => {
+                setConfirmPassword(evt.target.value);
+                if (evt.target.value === password) {
+                  setPasswordConfirmationError('');
+                }
+              }} />
+            {passwordConfirmationError &&
+              <div className="text-red-500">
+                { passwordConfirmationError }
+              </div>
+            }
           </div>
 
           <button type="submit" className="form-btn">
             Sign up
           </button>
+
+          {genericError &&
+            <div className="text-red-500 mt-2">
+              { genericError }
+            </div>
+          }
         </form>
       </section>
     </div>
